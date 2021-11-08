b033f17df0a74454cb4df988983540cd165e3058
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1460.local>
2021-11-08 11:06:54 +0000

    step 2 - classes

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // OOP
-let bunny = {
-    name: "Sherlock",
-    age: 2,
-    gender: "male",
-    faveFoods: ["carrots", "jelly beans"],
-    hops: 0,
+class Bunny {
+    constructor(a, b) {
+        this.name = a,
+        this.age = b,
+        this.hops = 0
+    }
 
     increaseHops () {
         this.hops++
-    },
+    }
 
     birthday () {
         this.age++
     }
 }
+
+let sherlock = new Bunny("Sherlock", 3)
+let watson = new Bunny("Watson", 2)
+
+console.log(sherlock)
+console.log(watson)
