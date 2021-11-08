b098245836513226da9307d8190c50834282db6b
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1460.local>
2021-11-08 10:58:00 +0000

    step 1 - objects

---
+++ app.js
@@ -0,0 +1,16 @@
+// OOP
+let bunny = {
+    name: "Sherlock",
+    age: 2,
+    gender: "male",
+    faveFoods: ["carrots", "jelly beans"],
+    hops: 0,
+
+    increaseHops () {
+        this.hops++
+    },
+
+    birthday () {
+        this.age++
+    }
+}
